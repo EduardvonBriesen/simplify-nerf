dc65a3ba4c6f177fbed77b4a18b0895783b0dd21
Eduard von Briesen <e.v.briesen@googlemail.com>
2024-01-08 11:35:40 +0100

    feat: ✨ multiple file upload

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,8 @@ const app = express();
 const server = require("http").createServer(app);
 import { spawn, exec } from "child_process";
 import cors from "cors";
-import fileUpload from "express-fileupload";
+import multer from "multer";
+import path from "path";
 
 const corsOptions = {
   origin: "*",
@@ -35,6 +36,45 @@ io.on("connection", (socket: any) => {
     });
   });
 
+  socket.on("download", () => {
+    console.log("Downloading file...");
+    const process = spawn(
+      "ns-download-data",
+      ["nerfstudio", "--capture-name=poster"],
+      {
+        cwd: "/workspace", // Set the working directory to /workspace
+      },
+    );
+    process.stdout.on("data", (data: any) => {
+      console.log("Sending data to client");
+      socket.emit("data", data.toString());
+    });
+  });
+
+  socket.on("train", () => {
+    console.log("Training model...");
+    process = spawn(
+      "ns-train",
+      ["nerfacto", " --data", "/workspace/data/nerfstudio/poster"],
+      {
+        cwd: "/workspace", // Set the working directory to /workspace
+      },
+    );
+    process.stdout.on("data", (data: any) => {
+      console.log("Sending data to client");
+      socket.emit("data", data.toString());
+    });
+
+    process.stderr.on("data", (data) => {
+      console.error("Sending stderr data to client");
+      socket.emit("data", data.toString());
+    });
+
+    process.on("close", (code) => {
+      console.log(`Child process exited with code ${code}`);
+    });
+  });
+
   // Handle client disconnection
   socket.on("disconnect", () => {
     console.log("Client disconnected");
@@ -47,7 +87,7 @@ io.on("connection", (socket: any) => {
 app.get("/run-rest", (req: any, res: any) => {
   console.log("Processing request...");
   exec(
-    "python3 ./test/app.py",
+    "ns-train nerfacto --data /workspace/data/nerfstudio/poster --output-dir /workspace/outputs",
     (error: { message: any }, stdout: any, stderr: any) => {
       if (error) {
         res.send(error.message);
@@ -63,24 +103,29 @@ app.get("/run-rest", (req: any, res: any) => {
   );
 });
 
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    safeFileNames: true,
-    preserveExtension: true,
-    tempFileDir: `./temp`,
-  }),
-);
+const storage = multer.diskStorage({
+  destination: "tmp/",
+  filename: (req, file, cb) => {
+    // Preserve original file name
+    cb(
+      null,
+      `${file.originalname}-${Date.now()}${path.extname(file.originalname)}`,
+    );
+  },
+});
 
-app.post("/upload", (req: any, res: any) => {
-  let uploadFile = req.files.file;
-  const name = uploadFile.name;
-  uploadFile.mv(`./temp/${name}`, function (err: any) {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    return res.status(200).json({ status: "uploaded", name });
-  });
+const upload = multer({ storage });
+
+app.post("/upload", upload.array("files"), (req, res) => {
+  // If using `upload.array('files')`, `req.files` will contain the uploaded files
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).json({ error: "No files were uploaded" });
+  }
+
+  // Process the uploaded files as needed
+  // Example: Move files, save file details to database, etc.
+
+  return res.status(200).json({ message: "Files uploaded successfully" });
 });
 
 // Start the server
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import socketIOClient from "socket.io-client";
+import { Terminal } from "xterm";
+import "xterm/css/xterm.css";
 
 function App() {
+  const terminalRef = useRef(null);
   const [socketData, setSocketData] = useState("");
   const [restData, setRestData] = useState("");
+  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
 
   const [socket, setSocket] = useState<any>(null);
 
   useEffect(() => {
-    setSocket(socketIOClient("http://localhost:3000"));
-  }, []);
+    const term = new Terminal();
+    if (terminalRef.current) {
+      term.open(terminalRef.current);
+    }
+    term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
 
-  const handleSocket = () => {
-    socket.emit("run");
+    const socket = socketIOClient("http://localhost:3000");
+    setSocket(socket);
 
-    socket.on("data", (data: any) => {
+    socket.on("data", (data: string) => {
       setSocketData(data);
+      term.write(data);
     });
 
     socket.on("disconnect", () => {
       console.log("Disconnected from server");
       socket?.removeAllListeners();
     });
-  };
+  }, []);
 
   const handleRest = () => {
     setRestData("");
@@ -33,35 +41,69 @@ function App() {
       });
   };
 
-  const handleFileUpload = (event: any) => {
+  const handleFileUpload = async (event: any) => {
     event.preventDefault();
-    const data = new FormData(event.target);
-    fetch("http://localhost:3000/upload", {
-      method: "POST",
-      body: data,
-    })
-      .then((response) => response.text())
-      .then((data) => {
-        console.log(data);
+
+    if (!selectedFiles) {
+      alert("Please select files to upload");
+      return;
+    }
+
+    const formData = new FormData();
+    for (let i = 0; i < selectedFiles.length; i++) {
+      formData.append("files", selectedFiles[i], selectedFiles[i].name);
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/upload", {
+        method: "POST",
+        body: formData,
       });
+
+      if (response.ok) {
+        // Handle success
+        console.log("Files uploaded successfully");
+      } else {
+        // Handle error
+        console.error("Failed to upload files");
+      }
+    } catch (error) {
+      console.error("Error occurred while uploading files:", error);
+    }
   };
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      {/* <div ref={terminalRef}></div> */}
       <div className="prose card w-4/5 bg-slate-700 p-8 shadow-xl">
         <h1>Python Output</h1>
-
         <h2>Socket.io</h2>
-        <button className="btn btn-primary w-min" onClick={handleSocket}>
-          Run
-        </button>
+        <div className="flex gap-4">
+          <button
+            className="btn btn-primary w-min"
+            onClick={() => socket.emit("run")}
+          >
+            Run
+          </button>
+          <button
+            className="btn btn-primary w-min"
+            onClick={() => socket.emit("download")}
+          >
+            Download
+          </button>
+          <button
+            className="btn btn-primary w-min"
+            onClick={() => socket.emit("train")}
+          >
+            Train
+          </button>
+        </div>
         <p>Socket.io output:</p>
         <div className="mockup-code">
           <pre data-prefix=">">
             <code>{socketData}</code>
           </pre>
         </div>
-
         <h2>REST</h2>
         <button className="btn btn-primary w-min" onClick={handleRest}>
           Run
@@ -73,12 +115,20 @@ function App() {
           </pre>
         </div>
 
+        {/* <iframe
+          src="https://viewer.nerf.studio/versions/23-05-15-1/?websocket_url=ws://localhost:7007"
+          title="Python"
+          className="h-96 w-full"
+        ></iframe> */}
+
         <h2>Upload File</h2>
         <form className="flex justify-evenly" onSubmit={handleFileUpload}>
           <input
             className="file-input file-input-bordered file-input-primary w-full max-w-xs"
             name="file"
             type="file"
+            multiple
+            onChange={(e) => setSelectedFiles(e.target.files)}
           />
           <button className="btn btn-primary" type="submit">
             Upload
